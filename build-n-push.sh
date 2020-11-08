8ae4cfe294551800a9ccfb23f00b801264983217
demian-wolf <demianwolfssd@gmail.com>
2020-11-09 01:28:28 +0200

    Updated build-n-push.sh script

--- build-n-push.sh
+++ build-n-push.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-rm -rf flask_src/build
-rm -rf flask_src/__pycache__
+
+cleanup () {
+    rm -rf flask_src/build
+    rm -rf flask_src/__pycache__
+}
+
+cleanup
 python3 flask_src/freeze.py
 rsync -avh --dry-run flask_src/build .
-git commit -a -m "Updated CodeClub page with new information"
+git commit -a -m "Updated build-n-push.sh script"
 git push
+cleanup
