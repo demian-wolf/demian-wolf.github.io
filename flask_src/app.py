42da8ad4fa81533bf39fccf10c4044b129d3d7f8
demian-wolf <demianwolfssd@gmail.com>
2022-04-28 11:19:57 +0300

    Add CV

--- flask_src/app.py
+++ flask_src/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from pathlib import Path
+
+from flask import Flask, render_template, send_from_directory
 from flask_minify import minify
 import yaml
-
 
 app = Flask(__name__)
 minify(app=app, html=True, js=True, cssless=True)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def index():
@@ -15,6 +17,14 @@ def index():
         return render_template("index.html",
                                index=data["index"],
                                portfolio=data["portfolio"])
+
+
+@app.route("/cv.pdf")
+def cv():
+    return send_from_directory(
+        Path(__file__).resolve().parent,
+        "cv.pdf"
+    )
 
 
 if __name__ == '__main__':
