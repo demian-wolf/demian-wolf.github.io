5b6c8406726b6924c5c5177715941b321dacddbb
Demian Volkov <demianwolfssd@gmail.com>
2022-10-25 18:36:46 +0100

    Move `flask_src/*` to the root

--- static/scripts/collapsible.js
+++ static/scripts/collapsible.js
@@ -1,1 +1,36 @@
-class Collapsible{constructor(element){this.element=element;const titleElement=element.querySelector(".collapsible__title");this.title=titleElement.innerText;titleElement.innerHTML=`<a class="collapsible__toggle_link"><span class="collapsible__arrow"></span>${this.title}</a>`;this.toggleLinkElement=titleElement.querySelector(".collapsible__toggle_link");this.toggleLinkElement.setAttribute("href","javascript:void(0);");this.toggleLinkElement.addEventListener("click",()=>this.toggle());this.arrowElement=this.toggleLinkElement.querySelector(".collapsible__arrow");this.contentElement=element.querySelector(".collapsible__content");this.setState(false);};setState(destState){this.contentElement.style.display=destState?"block":"none";this.arrowElement.innerHTML=destState?"\u25B2":"\u25BC";this.state=destState;};toggle(){this.setState(!this.state);}};document.addEventListener("DOMContentLoaded",()=>{Array.prototype.forEach.call(document.getElementsByClassName("collapsible"),collapsibleElement=>new Collapsible(collapsibleElement));});
+class Collapsible {
+    constructor(element) {
+        this.element = element;
+
+        const titleElement = element.querySelector(".collapsible__title");
+        this.title = titleElement.innerText;
+
+        titleElement.innerHTML = `<a class="collapsible__toggle_link"><span class="collapsible__arrow"></span>${this.title}</a>`;
+
+        this.toggleLinkElement = titleElement.querySelector(".collapsible__toggle_link");
+        this.toggleLinkElement.setAttribute("href", "javascript:void(0);");
+        this.toggleLinkElement.addEventListener("click", () => this.toggle());
+
+        this.arrowElement = this.toggleLinkElement.querySelector(".collapsible__arrow");
+        this.contentElement = element.querySelector(".collapsible__content");
+
+        this.setState(false);
+    }
+
+    setState(destState) {
+        this.contentElement.style.display = destState ? "block" : "none";
+        this.arrowElement.innerHTML = destState ? "\u25B2" : "\u25BC";
+
+        this.state = destState;
+    }
+
+    toggle() {this.setState(!this.state);}
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    Array.prototype.forEach.call(
+        document.getElementsByClassName("collapsible"),
+        collapsibleElement => new Collapsible(collapsibleElement)
+    );
+});
--- static/scripts/portfolio.js
+++ static/scripts/portfolio.js
@@ -1,1 +1,49 @@
-class TabManager{constructor(tabs){this.tabs=[];document.addEventListener("DOMContentLoaded",()=>{Array.prototype.forEach.call(tabs,tabElement=>tabManager.tabs.push(new Tab(tabElement)));this.tabs[0].goto();});};reset(){for(const tab of this.tabs)tab._hide();}};class Tab{constructor(element){this.element=element;this.buttonElement=document.createElement("a");this.buttonElement.classList.add("button","portfolio__tab_button");this.buttonElement.addEventListener("click",()=>this.goto());document.querySelector("#portfolio__tabs_buttons").appendChild(this.buttonElement);};_show(){this.element.style.display="block";this.buttonElement.classList.add("portfolio__tab_button_active");};_hide(){this.element.style.display="none";this.buttonElement.classList.remove("portfolio__tab_button_active");};goto(){tabManager.reset();this._show();}};tabManager=new TabManager(document.getElementsByClassName("portfolio__tab"));
+class TabManager {
+    constructor (tabs) {
+        this.tabs = [];
+        document.addEventListener("DOMContentLoaded", () => {
+            Array.prototype.forEach.call(
+                tabs,
+                tabElement => tabManager.tabs.push(new Tab(tabElement))
+            );
+            this.tabs[0].goto();
+        });
+    }
+
+    reset() {
+        for (const tab of this.tabs) tab._hide();
+    }
+}
+
+class Tab {
+    constructor(element) {
+        this.element = element;
+
+        this.buttonElement = document.createElement("a");
+        this.buttonElement.classList.add("button", "portfolio__tab_button");
+        this.buttonElement.addEventListener("click", () => this.goto());
+
+        document.querySelector(
+            "#portfolio__tabs_buttons"
+        ).appendChild(this.buttonElement);
+    }
+
+    _show() {
+        this.element.style.display = "block";
+        this.buttonElement.classList.add("portfolio__tab_button_active");
+    }
+
+    _hide() {
+        this.element.style.display = "none";
+        this.buttonElement.classList.remove("portfolio__tab_button_active");
+    }
+
+    goto() {
+        tabManager.reset();
+        this._show();
+    }
+}
+
+tabManager = new TabManager(
+    document.getElementsByClassName("portfolio__tab")
+);
