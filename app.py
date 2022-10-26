a6e6387c11d88c51c793f4968e36e3e8c1311c97
Demian Volkov <demianwolfssd@gmail.com>
2022-10-26 12:28:01 +0100

    Refactor templating

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 from flask import Flask, render_template, send_from_directory
 from flask_minify import minify
-import yaml
 
 app = Flask(__name__)
 minify(app=app, html=True, js=True, cssless=True)
@@ -12,18 +11,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-    with open("data.yaml") as fp:
-        data = yaml.safe_load(fp)
-        return render_template("index.html",
-                               index=data["index"],
-                               portfolio=data["portfolio"])
+    return render_template("index/index.html")
 
 
 @app.route("/cv.pdf")
 def cv():
     return send_from_directory(
         Path(__file__).resolve().parent,
-        "cv.pdf"
+        "static/portfolio/cv.pdf"
     )
 
 
